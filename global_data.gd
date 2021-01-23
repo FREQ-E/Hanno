ee234f9668d14c5fab4ba27fa2e3242a4fdbeb29
Manglemix <shabouza030@gmail.com>
2021-01-24 01:05:50 +0800

    remove inaccurate type hints

--- global_data.gd
+++ global_data.gd
@@ -3,10 +3,10 @@ extends Node
 
 signal player_changed
 
-var player: Character2D setget set_player
+var player: Node setget set_player
 var enemies: Array
 
 
-func set_player(node: Character2D) -> void:
+func set_player(node: Node) -> void:
 	player = node
 	emit_signal("player_changed")
