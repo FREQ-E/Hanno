767a8db62cc2a2c3e20a9999c78a0a79ba24de0d
Daniel <danielgranjac@gmail.com>
2021-02-01 19:35:43 +0000

    Fixed 4 direction-shooting

--- combat_scripts/player_shooting.gd
+++ combat_scripts/player_shooting.gd
@@ -9,19 +9,46 @@ export var fire_rate := 0.2
 export(PackedScene) var bullet_scene := preload("res://actors/player1/player_bullet.tscn")
 
 var can_fire := true
-
+var dir : Vector2
 
 func _process(_delta):
 	if not can_fire:
 		return
 	
-	var vector := Vector2(Input.get_action_strength("fire_right") - Input.get_action_strength("fire_left"), Input.get_action_strength("fire_down") - Input.get_action_strength("fire_up")).normalized()
-	
-	if vector.is_normalized():		# even though vector was normalized, it could still be length 0 if no key was pressed
+	if (Input.is_action_pressed("fire_up") or
+		Input.is_action_pressed("fire_left") or
+		Input.is_action_pressed("fire_down") or
+		Input.is_action_pressed("fire_right")) and can_fire:
+		
+		if (Input.is_action_pressed("fire_up")):
+			dir = Vector2(0,-1)
+		elif (Input.is_action_pressed("fire_left")):
+			dir = Vector2(-1,0)
+		elif (Input.is_action_pressed("fire_down")):
+			dir = Vector2(0,1)
+		elif (Input.is_action_pressed("fire_right")):
+			dir = Vector2(1,0)
+		
 		var bullet_instance = bullet_scene.instance()
 		bullet_instance.transform = global_transform
-		bullet_instance.linear_velocity = vector * bullet_speed
+		bullet_instance.linear_velocity = dir * bullet_speed
 		get_tree().current_scene.add_child(bullet_instance)
 		can_fire = false
 		yield(get_tree().create_timer(fire_rate), "timeout")
 		can_fire = true
+
+#func _process(_delta):
+#	if not can_fire:
+#		return
+#
+#	var vector := Vector2(Input.get_action_strength("fire_right") - Input.get_action_strength("fire_left"), Input.get_action_strength("fire_down") - Input.get_action_strength("fire_up")).normalized()
+#
+#
+#	if vector.is_normalized():		# even though vector was normalized, it could still be length 0 if no key was pressed
+#		var bullet_instance = bullet_scene.instance()
+#		bullet_instance.transform = global_transform
+#		bullet_instance.linear_velocity = vector * bullet_speed
+#		get_tree().current_scene.add_child(bullet_instance)
+#		can_fire = false
+#		yield(get_tree().create_timer(fire_rate), "timeout")
+#		can_fire = true
