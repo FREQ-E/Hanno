5ef70f30b973ca1573a425e2862835301ae930ae
Manglemix <shabouza030@gmail.com>
2021-02-02 11:59:53 +0800

    Removed legacy comment

--- combat_scripts/player_shooting.gd
+++ combat_scripts/player_shooting.gd
@@ -38,20 +38,3 @@ func _process(_delta):
 	get_tree().current_scene.add_child(bullet_instance)
 	can_fire = false
 	yield(get_tree().create_timer(fire_rate), "timeout")
-	can_fire = true
-
-#func _process(_delta):
-#	if not can_fire:
-#		return
-#
-#	var vector := Vector2(Input.get_action_strength("fire_right") - Input.get_action_strength("fire_left"), Input.get_action_strength("fire_down") - Input.get_action_strength("fire_up")).normalized()
-#
-#
-#	if vector.is_normalized():		# even though vector was normalized, it could still be length 0 if no key was pressed
-#		var bullet_instance = bullet_scene.instance()
-#		bullet_instance.transform = global_transform
-#		bullet_instance.linear_velocity = vector * bullet_speed
-#		get_tree().current_scene.add_child(bullet_instance)
-#		can_fire = false
-#		yield(get_tree().create_timer(fire_rate), "timeout")
-#		can_fire = true
