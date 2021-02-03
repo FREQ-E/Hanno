819fcce027cf30e14c430939f2d32445a9e037de
Manglemix <shabouza030@gmail.com>
2021-02-04 00:35:15 +0800

    Added crystal texture, and experimental navigation

--- actors/crystals/crystal_body.gd
+++ actors/crystals/crystal_body.gd
@@ -1,24 +1,14 @@
 class_name CrystalBody
-extends VisibilityNotifier2D
+extends Area2D
 
 
 export var value := 1
-
-var spawn_check := false			# set by the spawner, if true, will queue_free if was on screen when spawning
 
 onready var player: Node = GlobalFuncs.yield_and_get_group("Player")[0]
 
 
 func _ready():
-	if spawn_check:
-		yield(get_tree(), "idle_frame")
-		
-		if is_on_screen():
-			queue_free()
-			return
-	
-	# warning-ignore:return_value_discarded
-	$Area2D.connect("body_entered", self, "_handle_body_entered")
+	connect("body_entered", self, "_handle_body_entered")
 
 
 func _handle_body_entered(body: Node) -> void:
