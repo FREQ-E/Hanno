68afa934f4e36d95110d639bfea6284c03c98eaf
Manglemix <shabouza030@gmail.com>
2021-02-27 13:08:16 +0800

    Added snake, and improved CharacterAnimator

--- actors/character_animator.gd
+++ actors/character_animator.gd
@@ -3,6 +3,7 @@ extends AnimatedSprite
 
 
 export var default_movement_speed := 770.0				# The speed the parent has to move at such that the speed_scale is 1. Moving faster would cause the animation speed to speed up, and vice versa
+export var invert_x := false
 
 onready var _last_origin: Vector2 = get_parent().global_transform.origin
 
@@ -13,19 +14,19 @@ func _process(delta):
 		frame = 1
 	
 	elif delta > 0:
-		var local: Vector2 = global_transform.basis_xform_inv(get_parent().movement_vector)
+		var angle := global_transform.basis_xform_inv(get_parent().movement_vector).angle()
 		var new_origin: Vector2 = get_parent().global_transform.origin
 		speed_scale = new_origin.distance_to(_last_origin) / delta / default_movement_speed
 		_last_origin = new_origin
 		
-		if local.y > 0:
-			play("walk_down")
-		
-		elif local.y < 0:
+		if angle < - PI / 4 and angle > - 3 * PI / 4:
 			play("walk_up")
+		
+		elif angle > PI / 4 and angle < 3 * PI / 4:
+			play("walk_down")
 		
 		else:
 			play("walk_side")
 			
-			if local.x < 0:
+			if abs(angle) > PI / 2 != invert_x:
 				global_transform.x *= -1
